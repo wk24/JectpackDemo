d95fb72f69e339144c900a7c0e2bc2edc2b7714b
wukun <1779282511@qq.com>
2021-11-17 17:00:16 +0800

    init

---
+++ livedatademo/src/main/java/com/wuk/livedatademo/simple1/MyLiveData.kt
@@ -0,0 +1,11 @@
+package com.wuk.livedatademo.simple1
+
+import androidx.lifecycle.MutableLiveData
+
+
+object MyLiveData { // 单例
+
+    // 懒加载
+    val info1: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+
+}
